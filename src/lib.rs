c8fb9161505733718c36ba1aed2259b90b70d185
Sylwester Rąpała <sylwesterrapala@outlook.com>
2022-01-26 00:04:15 +0100

    Test codecoverage

--- src/lib.rs
+++ src/lib.rs
@@ -11,11 +11,18 @@
 #![deny(missing_docs)]
 #![warn(clippy::missing_docs_in_private_items)]
 
+/// return 2
+pub fn return2() -> i32 {
+    2_i32
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::return2;
+
     #[test]
     fn it_works() {
-        let x = 2;
+        let x = return2();
 
         assert_eq!(2 + x, 4);
     }
